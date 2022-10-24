4342242919e66877ea531c7c8eed6444e12e7e98
Kerven GUrbangulyev <atashka1995@gmail.com>
2022-10-24 12:11:22 +0500

    sigin post method

--- routes/signin.js
+++ routes/signin.js
@@ -4,24 +4,24 @@ let DatabaseManager = require("../monitoring/DatabaseManager");
 
 router.post('/', function (req, res, next) {
   console.log("sign post method works!");
-  console.log(req.body);
-  if (req.body.login != undefined && req.body.password != undefined) {
-    let status = DatabaseManager.getStatus(req.body.login, req.body.password);
-    if (status == 'user')
-    {
-      let cookie = DatabaseManager.setCookieForUser(req.body.login, req.body.password);
-      if(cookie!=null)
-      {
-       res.cookie("id", cookie);
-       res.redirect("/stretching");
-      }
-      else
-        res.redirect("/signin");
-    }
-    else {
-      res.redirect('/signin');
-    }
-  }
+  // console.log(req.body);
+  // if (req.body.login != undefined && req.body.password != undefined) {
+  //   let status = DatabaseManager.getStatus(req.body.login, req.body.password);
+  //   if (status == 'user')
+  //   {
+  //     let cookie = DatabaseManager.setCookieForUser(req.body.login, req.body.password);
+  //     if(cookie!=null)
+  //     {
+  //      res.cookie("id", cookie);
+  //      res.redirect("/stretching");
+  //     }
+  //     else
+  //       res.redirect("/signin");
+  //   }
+  //   else {
+  //     res.redirect('/signin');
+  //   }
+  // }
   res.redirect("/registration");
 });
 /* GET users listing. */
